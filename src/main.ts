53ee1bdb491de0894859d6ed58ad21feee8295f6
Misael Burboa <mburboa@consumertrack.com>
2021-10-26 08:35:40 -0700

    enable helmet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './modules/app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // INFO: Setup middlewares
+  app.use(helmet());
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Horizonte Hermosillo Api')
